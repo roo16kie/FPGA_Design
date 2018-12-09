d456ddac5eea6d864ffb9e52fcc69395b1f0f10d
nicolashei1201 <n861201@gmail.com>
2018-12-09 22:10:40 +0800

    HW6_V1

---
+++ Lab06/hw/E24053037/HW6_project/HW6.sdk/hw6_bsp/ps7_cortexa9_0/include/matrix_cal.h
@@ -0,0 +1,91 @@
+
+#ifndef MATRIX_CAL_H
+#define MATRIX_CAL_H
+
+
+/****************** Include Files ********************/
+#include "xil_types.h"
+#include "xstatus.h"
+
+#define MATRIX_CAL_S00_AXI_SLV_REG0_OFFSET 0
+#define MATRIX_CAL_S00_AXI_SLV_REG1_OFFSET 4
+#define MATRIX_CAL_S00_AXI_SLV_REG2_OFFSET 8
+#define MATRIX_CAL_S00_AXI_SLV_REG3_OFFSET 12
+#define MATRIX_CAL_S00_AXI_SLV_REG4_OFFSET 16
+#define MATRIX_CAL_S00_AXI_SLV_REG5_OFFSET 20
+#define MATRIX_CAL_S00_AXI_SLV_REG6_OFFSET 24
+#define MATRIX_CAL_S00_AXI_SLV_REG7_OFFSET 28
+#define MATRIX_CAL_S00_AXI_SLV_REG8_OFFSET 32
+#define MATRIX_CAL_S00_AXI_SLV_REG9_OFFSET 36
+#define MATRIX_CAL_S00_AXI_SLV_REG10_OFFSET 40
+#define MATRIX_CAL_S00_AXI_SLV_REG11_OFFSET 44
+#define MATRIX_CAL_S00_AXI_SLV_REG12_OFFSET 48
+#define MATRIX_CAL_S00_AXI_SLV_REG13_OFFSET 52
+#define MATRIX_CAL_S00_AXI_SLV_REG14_OFFSET 56
+#define MATRIX_CAL_S00_AXI_SLV_REG15_OFFSET 60
+
+
+/**************************** Type Definitions *****************************/
+/**
+ *
+ * Write a value to a MATRIX_CAL register. A 32 bit write is performed.
+ * If the component is implemented in a smaller width, only the least
+ * significant data is written.
+ *
+ * @param   BaseAddress is the base address of the MATRIX_CALdevice.
+ * @param   RegOffset is the register offset from the base to write to.
+ * @param   Data is the data written to the register.
+ *
+ * @return  None.
+ *
+ * @note
+ * C-style signature:
+ * 	void MATRIX_CAL_mWriteReg(u32 BaseAddress, unsigned RegOffset, u32 Data)
+ *
+ */
+#define MATRIX_CAL_mWriteReg(BaseAddress, RegOffset, Data) \
+  	Xil_Out32((BaseAddress) + (RegOffset), (u32)(Data))
+
+/**
+ *
+ * Read a value from a MATRIX_CAL register. A 32 bit read is performed.
+ * If the component is implemented in a smaller width, only the least
+ * significant data is read from the register. The most significant data
+ * will be read as 0.
+ *
+ * @param   BaseAddress is the base address of the MATRIX_CAL device.
+ * @param   RegOffset is the register offset from the base to write to.
+ *
+ * @return  Data is the data from the register.
+ *
+ * @note
+ * C-style signature:
+ * 	u32 MATRIX_CAL_mReadReg(u32 BaseAddress, unsigned RegOffset)
+ *
+ */
+#define MATRIX_CAL_mReadReg(BaseAddress, RegOffset) \
+    Xil_In32((BaseAddress) + (RegOffset))
+
+/************************** Function Prototypes ****************************/
+/**
+ *
+ * Run a self-test on the driver/device. Note this may be a destructive test if
+ * resets of the device are performed.
+ *
+ * If the hardware system is not built correctly, this function may never
+ * return to the caller.
+ *
+ * @param   baseaddr_p is the base address of the MATRIX_CAL instance to be worked on.
+ *
+ * @return
+ *
+ *    - XST_SUCCESS   if all self-test code passed
+ *    - XST_FAILURE   if any self-test code failed
+ *
+ * @note    Caching must be turned off for this function to work.
+ * @note    Self test may fail if data memory and device are not on the same bus.
+ *
+ */
+XStatus MATRIX_CAL_Reg_SelfTest(void * baseaddr_p);
+
+#endif // MATRIX_CAL_H
